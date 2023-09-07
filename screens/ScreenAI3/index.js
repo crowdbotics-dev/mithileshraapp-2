7282bbac663caf871e1b193cc9682cb4407d3a66
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-09-07 13:51:30 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,75 +1,27 @@
-import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, View, Text, Image, TextInput, TouchableOpacity, CheckBox } from "react-native";
+import React from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
-const LoginScreen = () => {
-  const [isSelected, setSelection] = useState(false);
+const ThreeDotsScreen = () => {
   return <SafeAreaView style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={{
-        uri: "https://tinyurl.com/42evm3m3"
-      }} />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Username" />
-        <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-        <View style={styles.checkboxContainer}>
-          <CheckBox value={isSelected} onValueChange={setSelection} style={styles.checkbox} />
-          <Text style={styles.label}>Save Password</Text>
-        </View>
-      </View>
-      <TouchableOpacity style={styles.buttonContainer}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+      <View style={styles.dot} />
+      <View style={styles.dot} />
+      <View style={styles.dot} />
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  logoContainer: {
-    marginBottom: 50
-  },
-  logo: {
-    width: 100,
-    height: 100
-  },
-  inputContainer: {
-    width: "80%",
-    marginBottom: 20
-  },
-  input: {
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    borderRadius: 5
-  },
-  buttonContainer: {
-    width: "80%",
-    height: 40,
-    backgroundColor: "#007BFF",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 5
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16
-  },
-  checkboxContainer: {
     flexDirection: "row",
-    marginBottom: 20
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5"
   },
-  checkbox: {
-    alignSelf: "center"
-  },
-  label: {
-    margin: 8
+  dot: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: "green"
   }
 });
-export default LoginScreen;
+export default ThreeDotsScreen;
--- screens/ScreenAI4/index.js
+++ screens/ScreenAI4/index.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, Button, Image, View } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button, Image, View } from "react-native";
 
 const LoginScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {// Handle login logic here
   };
 
   return <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={{
-      uri: 'https://tinyurl.com/42evm3m3'
+      uri: "https://tinyurl.com/42evm3m3"
     }} />
       <TextInput style={styles.input} onChangeText={setUsername} value={username} placeholder="Username" />
       <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="Password" secureTextEntry />
@@ -23,9 +23,9 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red"
   },
   logo: {
     width: 150,
@@ -34,15 +34,15 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    width: '80%',
-    borderColor: 'gray',
+    width: "80%",
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 20,
     paddingLeft: 10,
     borderRadius: 5
   },
   buttonContainer: {
-    width: '80%',
+    width: "80%",
     marginTop: 20
   }
 });
