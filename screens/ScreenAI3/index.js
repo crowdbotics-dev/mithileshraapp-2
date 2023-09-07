f9b29279f3e357575f8b87c85c4130f51560bf72
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-09-07 13:49:13 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, View, Text, Image, TextInput, TouchableOpacity, CheckBox } from "react-native";
 
 const LoginScreen = () => {
+  const [isSelected, setSelection] = useState(false);
   return <SafeAreaView style={styles.container}>
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={{
-        uri: 'https://tinyurl.com/42evm3m3'
+        uri: "https://tinyurl.com/42evm3m3"
       }} />
       </View>
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} placeholder="Username" />
         <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+        <View style={styles.checkboxContainer}>
+          <CheckBox value={isSelected} onValueChange={setSelection} style={styles.checkbox} />
+          <Text style={styles.label}>Save Password</Text>
+        </View>
       </View>
       <TouchableOpacity style={styles.buttonContainer}>
         <Text style={styles.buttonText}>Login</Text>
@@ -21,9 +26,9 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center"
   },
   logoContainer: {
     marginBottom: 50
@@ -33,28 +38,38 @@ const styles = StyleSheet.create({
     height: 100
   },
   inputContainer: {
-    width: '80%',
+    width: "80%",
     marginBottom: 20
   },
   input: {
     height: 40,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
     borderRadius: 5
   },
   buttonContainer: {
-    width: '80%',
+    width: "80%",
     height: 40,
-    backgroundColor: '#007BFF',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#007BFF",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 5
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20
+  },
+  checkbox: {
+    alignSelf: "center"
+  },
+  label: {
+    margin: 8
   }
 });
 export default LoginScreen;
